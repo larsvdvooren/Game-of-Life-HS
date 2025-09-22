e7cbd92d8689d63fd6791d265c943fdab4ba4ba0
larsvdvooren <74357425+larsvdvooren@users.noreply.github.com>
2025-09-22 13:46:18 +0200

    initial commit
    barebones for now

---
+++ DOCUMENTATION.md
@@ -0,0 +1,20 @@
+
+Gloss:
+MSYS2:
+> pacman -Syu
+> pacman -S mingw-w64-x86_64-freeglut
+
+
+handmatig aan path toevoegen
+freeglut downloaden [link ooit] dll in path zetten
+
+
+3 verschillende repo's inspiratie uithalen en eigen intepretatie.
+  - wie doet wat en waarom?
+  - Waarom kies ik optie a over b of c?
+  - Doe ik iets anders? waarom?
+
+3 verschillende staten van gridcells:
+- Levend ()
+- Dood (voormalig levend)
+- Levenloos (nooit levend of dood geweest)
--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,51 @@
 module Main where
 
+import Graphics.Gloss
+import Graphics.Gloss.Interface.Pure.Game
+
+
+-- window and gameField size (gridheight is based on gridwidth, can be decoupled if required)
+windowWidth = 1000
+windowHeight = windowWidth
+-- //windowHeight = 1000
+gridWidth = 40 -- amount of cells the grid is wide
+gridHeight = gridWidth
+-- //gridHeight = 40
+padding = 100
+
+-- further sizes are calculated based on above values
+fieldWidth = windowWidth - padding -- the width of the actual 'playfield' 
+fieldHeight = windowHeight - padding  -- " height "
+-- cell size is based on the size of the field divided by how many cells there are, width and height are separate values
+cellWidth = (fieldWidth - padding) / gridWidth 
+cellHeight = (fieldHeight - padding) / gridHeight
+
+-- life values (dead is former alive, lifeless has never been alive)
+aliveCell = green
+deadCell = greyN 0.8
+lifelessCell = black
+
+
+
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+    putStrLn "henlo"
+
+
+
+-- window = InWindow "Game of Life" (width, height) (0,0)
+
+
+-- -- padding = 25
+
+-- w = (fromIntegral width) - padding
+-- h = (fromIntegral height) - padding
+
+
+-- cell = rectangleSolid (w/x) (h/y)
+-- grid = rectangleSolid (w) (h)
+
+-- x = 50
+-- y = 50
+
